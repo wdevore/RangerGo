a5b6c3e25d56b3c8791f853167762c6c4d932b2b
William Cleveland <iposthuman@pop-os.localdomain>
2020-04-25 11:47:28 -0400

    added NodeToNode and NodeToWorld mappings

--- engine/nodes/space_mappings.go
+++ engine/nodes/space_mappings.go
@@ -1,8 +1,6 @@
 package nodes
 
 import (
-	"fmt"
-
 	"github.com/wdevore/RangerGo/api"
 	"github.com/wdevore/RangerGo/engine/geometry"
 	"github.com/wdevore/RangerGo/engine/maths"
@@ -21,7 +19,7 @@ func MapDeviceToView(world api.IWorld, dvx, dvy int32, viewPoint api.IPoint) {
 // Optional scaling before returning from function. Extremely rare to use
 // localPoint.SetByComp(localPoint.X() * node.Scale(), localPoint.Y() * node.Scale())
 func MapDeviceToNode(dvx, dvy int32, node api.INode, localPoint api.IPoint) {
-	// Mapping from device to node requires to transforms from to "directions"
+	// Mapping from device to node requires transforms from two "directions"
 	// 1st is upwards transform and the 2nd is downwards transform.
 
 	// downwards from device-space to view-space
@@ -34,6 +32,35 @@ func MapDeviceToNode(dvx, dvy int32, node api.INode, localPoint api.IPoint) {
 	wtn.TransformCompToPoint(tViewPoint.X(), tViewPoint.Y(), localPoint)
 }
 
+// MapNodeToNode maps node's local origin (0,0) to another node's space
+// Supplying a psuedo-root can reduce multiplications, thus use it if possible.
+func MapNodeToNode(sourceNode, destinationNode api.INode, nodePoint api.IPoint, psuedoRoot api.INode) {
+	ntw := NodeToWorldTransform(sourceNode, psuedoRoot)
+	ntw.TransformCompToPoint(0.0, 0.0, nodePoint)
+	// nodePoint is now in world-space
+
+	wtn := WorldToNodeTransform(destinationNode, psuedoRoot) // nodePoint
+	wtn.TransformCompToPoint(nodePoint.X(), nodePoint.Y(), nodePoint)
+	// nodePoint is now in the destination node's space
+
+	// OR via view-space (not recommended as it is extra operations and rounding)
+	// MapNodeToDevice(sourceNode.World(), sourceNode, nodePoint)
+	// MapDeviceToNode(int32(nodePoint.X()), int32(nodePoint.Y()), destinationNode, nodePoint)
+}
+
+// MapNodeToWorld maps node's local origin to world-space
+func MapNodeToWorld(node api.INode, viewPoint api.IPoint) {
+	ntw := NodeToWorldTransform(node, nil)
+	ntw.TransformCompToPoint(0.0, 0.0, viewPoint)
+}
+
+// MapNodeToDevice maps node local origin to device-space (aka mouse-space)
+func MapNodeToDevice(world api.IWorld, node api.INode, viewPoint api.IPoint) {
+	ntw := NodeToWorldTransform(node, nil)
+	ntw.TransformCompToPoint(0.0, 0.0, viewPoint)
+	world.ViewSpace().TransformCompToPoint(viewPoint.X(), viewPoint.Y(), viewPoint)
+}
+
 // WorldToNodeTransform maps a world-space coordinate to local-space of node
 func WorldToNodeTransform(node api.INode, psuedoRoot api.INode) api.IAffineTransform {
 	wtn := NodeToWorldTransform(node, psuedoRoot)
@@ -43,10 +70,8 @@ func WorldToNodeTransform(node api.INode, psuedoRoot api.INode) api.IAffineTrans
 
 // NodeToWorldTransform maps a local-space coordinate to world-space
 func NodeToWorldTransform(node api.INode, psuedoRoot api.INode) api.IAffineTransform {
-	aft := node.CalcTransform()
-
 	// A transform to accumulate the parent transforms.
-	comp.SetByTransform(aft)
+	comp.SetByTransform(node.CalcTransform())
 
 	// Iterate "upwards" starting with the child towards the parents
 	// starting with this child's parent.
@@ -76,7 +101,7 @@ func NodeToWorldTransform(node api.INode, psuedoRoot api.INode) api.IAffineTrans
 		comp.SetByTransform(out)
 
 		if p == psuedoRoot {
-			fmt.Println("SpaceMappings: hit psuedoRoot")
+			// fmt.Println("SpaceMappings: hit psuedoRoot")
 			break
 		}
 
